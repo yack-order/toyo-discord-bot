01ca7d44d9c35768a463622d0f443fd60573522c
flyingtoasters <humor4fun@users.noreply.github.com>
2025-03-26 12:19:43 -0400

    initial bot commit

---
+++ toyobot/src/commands.js
@@ -0,0 +1,10 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const AWW_COMMAND = {
+  name: 'awwww',
+  description: 'Drop some cuteness on this channel.',
+};
+
